c0796de32368cbcc6fecb3bd0e08ce0187a4f67f
MezhovMV <mmezhov@gmail.com>
2025-03-23 15:35:58 +0000

    Added foles search

---
+++ XMLCleaner/XMLCleaner/main.cpp
@@ -0,0 +1,69 @@
+#pragma once
+#include <regex>
+#include "Cleaner.h"
+#include "Test.h"
+#include <stdexcept>
+
+namespace fs = std::filesystem;
+
+static bool Found(fs::path path, std::regex rx) {
+    return std::regex_match(path.string(), rx);
+}
+
+
+int main(int argc, char* argv[])
+{
+   
+    RunTests();
+    std::string path;
+    std::cout << "Please write directory path" << std::endl;
+    std::cout << "For example : current path is " << fs::current_path() << std::endl;
+    while (true) {
+        try { std::cin >> path; 
+        std::regex rx(".*\\.vcxproj$");
+        std::ofstream log;
+        log.open("log.txt");
+        
+        if (!fs::is_directory(path) && Found(path,rx)) {
+            fs::path file = path;
+
+            if (!IsXml(file)) {
+                log << file.filename() << " is not XML" << std::endl;
+            }
+            if (IsXml(file) && (!IsProject(file))) {
+                log << file.filename() << " is not VsProject" << std::endl;
+            }
+            if (IsXml(file) && IsProject(file)) {
+                CleanWin32(file);
+                log << file.filename() << " is cleaned" << std::endl;
+            }
+            std::cout << "Program has finished succesfully, log is saved in program direcory";
+            break;
+        }
+
+        for (const auto& p : fs::recursive_directory_iterator(path.c_str())) {
+            if (!fs::is_directory(p)) {
+                if (Found(p, rx) && (!IsXml(p))) {
+                    log << p.path().filename() << " is not XML" << std::endl;
+                }
+                if (Found(p, rx) && IsXml(p) && (!IsProject(p))) {
+                    log << p.path().filename() << " is not VsProject" << std::endl;
+                }
+                if (Found(p, rx) && IsXml(p) && IsProject(p)) {
+                    CleanWin32(p);
+                    log << p.path().filename() << " is cleaned" << std::endl;
+                }
+
+            }
+        }
+
+        std::cout << "Program has finished succesfully, log is saved in program direcory";
+        break;
+        }
+        catch (fs::filesystem_error &ex) {
+            std::cout << "Invalid path" << std::endl;
+        }
+    }; 
+ 
+
+}
